5ec7dff8e79d3764e776fe660f896818a2adabc5
aizhamal <amanay2005@mail.ru>
2023-11-20 00:41:52 +0600

    update database

--- add-watches.py
+++ add-watches.py
@@ -1,16 +1,18 @@
 from flaskapp import app
 from database.models import db, Product
 
+try:
+    with app.app_context():
+        with app.app_context():
+            watches = [
+                Product(name="Classic Watch", description="hello", price=250.0),
+                Product(name="Modern Watch", price=300.0),
+                Product(name="Vintage Watch", price=200.0)
+                # Add more watches as needed
+            ]
+        db.session.add_all(watches)
+        db.session.commit()
+except Exception as e:
+    print(f"Error adding watches to the database: {e}")
 
-with app.app_context():
-    watches = [
-    Product(name="Classic Watch", price=250.0),
-    Product(name="Modern Watch", price=300.0),
-    Product(name="Vintage Watch", price=200.0)
-    # Add more watches as needed
-]
 
-    db.session.add_all(watches)
-    db.session.commit()
-
-print("Watches added to the database.")
--- database/models.py
+++ database/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     __tablename__ = "users"
     user_id = db.Column(db.Integer, primary_key=True)
@@ -11,13 +12,22 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     cart_items = db.relationship('CartItem', backref='user', lazy=True)
 
-class CartItem(db.Model):
-    __tablename__ = "cart_items"
-    item_id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.String(255))
-    price = db.Column(db.Float)
-    quantity = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    def __init__(self, login, user_fname, user_sname, password):
+        self.login = login
+        self.user_fname = user_fname
+        self.user_sname = user_sname
+        self.password = password
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {"login": self.login,
+                "user_fname": self.user_fname,
+                "user_sname": self.user_sname
+                }
+
 
 class Product(db.Model):
     __tablename__ = "products"
@@ -25,6 +35,50 @@ class Product(db.Model):
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
     price = db.Column(db.Float, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+    def __init__(self, img_url, name, price, description):
+        self.img_url = img_url
+        self.name = name
+        self.price = price
+        self.description = description
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def deleteFromDB(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {"id": self.product_id,
+                "name": self.name,
+                "img_url": self.img_url,
+                "description": self.description,
+                "price": self.price}
 
 
+class CartItem(db.Model):
+    __tablename__ = "cart_items"
+    item_id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
+    def __init__(self, user_id, product_id, quantity=1):
+        self.user_id = user_id
+        self.mug_id = product_id
+        self.quantity = quantity
+
+    def update_quantity(self, quantity):
+        self.quantity += quantity
+        db.session.commit()
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def deleteFromDB(self):
+        db.session.delete(self)
+        db.session.commit()
