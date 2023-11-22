5ee10bf01fdc6da3ed3b134939c53d40e493e4b5
aizhamal <amanay2005@mail.ru>
2023-11-22 15:46:48 +0600

    commit

--- database/crud.py
+++ database/crud.py
@@ -5,9 +5,11 @@ def add_user(user:User)->None:
     db.session.add(user)
     db.session.commit()
 
+
 def delete_user(user:User)->None:
     db.session.delete(user)
     db.session.commit()
+
 
 def get_all_users()->db.Query:
     return User.query.all()
